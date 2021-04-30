17b2107fcccdc5911ba36b14cb01e174b2f08ddf
HAIDAR <saad.haidar@capgemini.com>
2021-04-30 02:52:25 +0200

    update project

--- src/main/java/com/fsdm/crm/exception/GlobalDefaultExceptionHandler.java
+++ src/main/java/com/fsdm/crm/exception/GlobalDefaultExceptionHandler.java
@@ -7,7 +7,6 @@ import java.util.List;
 import java.util.Map;
 
 import javax.servlet.http.HttpServletRequest;
-import javax.validation.ValidationException;
 
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
